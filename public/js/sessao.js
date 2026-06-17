3d4413d0fcfbbc2e9aa80603d504e0c284421201
matehus064 <matheus.drosa@sptech.school>
2026-06-17 01:40:47 -0300

    add: foto de perfil

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,11 +2,15 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var fotoPerfil = sessionStorage.FOTO_PERFIL;
 
     var b_usuario = document.getElementById("b_usuario");
+    var b_fotoPerfil = document.getElementById("b_fotoPerfil");
 
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
+        b_fotoPerfil.src = `../assets/imgs/profilePics/${fotoPerfil}.webp`;
+        
     } else {
         window.location = "../login.html";
     }
