c6ce075b2eca7ad5ceed7421c7ab9f790e76e132
matehus064 <matheus.drosa@sptech.school>
2026-07-02 14:02:31 -0300

    fix: scripts pkmnHub_lite

---
+++ src/database/script-pkmnHub_lite.sql
@@ -0,0 +1,49 @@
+create database if not exists pkmnHub_lite;
+
+use pkmnHub_lite;
+
+create table usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL UNIQUE,
+    email VARCHAR(65) NOT NULL UNIQUE,
+    senha VARCHAR(255),
+    foto_perfil INT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table transacao (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_usuario INT,
+    tipo_movimentacao ENUM('compra', 'venda', 'troca'),
+    valor_transacao DECIMAL(10, 2),
+    data_movimento DATE DEFAULT (CURRENT_DATE), 
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+create table snapshot_colecao (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_usuario INT,
+    valor_total DECIMAL(10, 2),
+    data_snapshot DATE DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+create table cartas (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_carta VARCHAR(45) NOT NULL,
+    nome_set VARCHAR(45),
+    numero_set VARCHAR(10),
+    url_imagem VARCHAR(255)
+);
+
+create table colecao (
+    fk_usuario INT,
+    fk_carta INT,
+    quantidade INT DEFAULT 1,
+    preco_compra DECIMAL(10, 2),
+    preco_ligaPkmn DECIMAL(10, 2),
+    data_adicao DATE DEFAULT (CURRENT_DATE),
+    PRIMARY KEY (fk_usuario, fk_carta),
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (fk_carta) REFERENCES cartas(id)
+);
