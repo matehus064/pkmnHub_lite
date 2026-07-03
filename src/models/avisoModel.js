3ea755e965b34f76441cb6ec48a6c657d2af8080
matehus064 <matheus.drosa@sptech.school>
2026-07-02 21:49:40 -0300

    add: models para a função de cadastro de cartas e snapshots

--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -89,11 +89,79 @@ function deletar(idAviso) {
     return database.executar(instrucaoSql);
 }
 
+function existeCarta(nomeCarta, numeroSet) {
+    var instrucaoSql = `
+        SELECT id, nome_carta, numero_set FROM cartas WHERE nome_carta = '${nomeCarta}' AND numero_set = '${numeroSet}';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function cadastrar(nomeCarta, nomeSet, numeroSet, urlImagem) { //OK
+    var instrucaoSql = `
+        INSERT INTO cartas (nome_carta, nome_set, numero_set, url_imagem) VALUES ('${nomeCarta}', '${nomeSet}', '${numeroSet}', '${urlImagem}');
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function adicionarNaColecao(usuario, carta, quantidade, precoCompra, precoLigaPkmn) {
+    var instrucaoSql = `
+        INSERT INTO colecao (fk_usuario, fk_carta, quantidade, preco_compra, preco_ligaPkmn) VALUES ('${usuario}', '${carta}', '${quantidade}', '${precoCompra}', '${precoLigaPkmn}');
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function verificarCartaNaColecaoPorId(usuario, carta) {
+    var instrucaoSql = `
+        SELECT quantidade FROM colecao WHERE fk_usuario = '${usuario}' AND fk_carta = '${carta}';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function somarQuantidadeCompra(usuario, carta, quantidade, precoCompra, precoLigaPkmn) {
+    var instrucaoSql = `
+        UPDATE colecao 
+        SET quantidade = quantidade + ${quantidade}, 
+            preco_compra = '${precoCompra}', 
+            preco_ligaPkmn = '${precoLigaPkmn}'
+        WHERE fk_usuario = '${usuario}' AND fk_carta = '${carta}';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function registrarTransacao(usuario, tipoMovimentacao, valorVenda) {
+    var instrucaoSql = `
+        INSERT INTO transacao (fk_usuario, tipo_movimentacao, valor_transacao) VALUES ('${usuario}', '${tipoMovimentacao}', '${valorVenda}');
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function buscarValorTotalColecao(usuario) {
+    var instrucaoSql = `
+        SELECT SUM(c.preco_ligaPkmn * c.quantidade) AS valor_total_colecao FROM colecao c JOIN usuario u ON u.id = c.fk_usuario WHERE c.fk_usuario = '${usuario}';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function salvarSnapshot(usuario, valorTotal) {
+    var instrucaoSql = `
+        INSERT INTO snapshots_colecao (fk_usuario, valor_total) VALUES ('${usuario}', '${valorTotal}');
+    `;
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     listar,
     listarPorUsuario,
     pesquisarDescricao,
     publicar,
     editar,
-    deletar
+    deletar,
+    existeCarta,
+    cadastrar,
+    adicionarNaColecao,
+    verificarCartaNaColecaoPorId,
+    somarQuantidadeCompra,
+    registrarTransacao,
+    buscarValorTotalColecao,
+    salvarSnapshot
 }
