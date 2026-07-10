bd21f5bc3555168512e797860f8287ffc119118d
matehus064 <matheus.drosa@sptech.school>
2026-07-10 17:59:24 -0300

    fix: adaptando as funções de plotar gráfico de linhas (atualizarGrafico, plotarGrafico e obterDadosGrafico)

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,27 +1,14 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
-                        temperatura,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
-
+function buscarUltimasMedidas(usuario, limite_linhas) {
+    var instrucaoSql = `SELECT data_snapshot, valor_total FROM snapshot_colecao WHERE fk_usuario = ${usuario} ORDER BY id DESC LIMIT ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(usuario) {
 
-    var instrucaoSql = `SELECT 
-                        temperatura,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    var instrucaoSql = `SELECT data_snapshot, valor_total FROM snapshot_colecao WHERE fk_usuario = ${usuario} ORDER BY id DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
