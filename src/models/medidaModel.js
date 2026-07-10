ac7992be9b7de3a59101d2e87321055ad7cc9f9f
matehus064 <matheus.drosa@sptech.school>
2026-07-10 18:17:53 -0300

    add: models para as kpis da dashboard

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -14,7 +14,42 @@ function buscarMedidasEmTempoReal(usuario) {
     return database.executar(instrucaoSql);
 }
 
+function buscarTotalCartas(usuario) {
+    var instrucaoSql = `
+        SELECT SUM(quantidade) AS total_cartas_fisicas FROM colecao WHERE fk_usuario = ${usuario};
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function buscarTotalCompra(usuario) {
+    var instrucaoSql = `
+        SELECT SUM(valor_transacao) AS total_compra FROM transacao WHERE fk_usuario = ${usuario} AND tipo_movimentacao = 'compra';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function buscarTotalVenda(usuario) {
+    var instrucaoSql = `
+        SELECT SUM(valor_transacao) AS total_venda FROM transacao WHERE fk_usuario = ${usuario} AND tipo_movimentacao = 'venda';
+    `;
+    return database.executar(instrucaoSql);
+}
+
+function buscarCartaMaisCara(usuario) {
+    var instrucaoSql = `
+        SELECT b.nome_carta, b.nome_set, b.url_imagem, c.preco_ligaPkmn FROM colecao AS c 
+        JOIN cartas AS b ON c.fk_carta = b.id 
+        WHERE c.fk_usuario = ${usuario}
+        ORDER BY c.preco_ligaPkmn DESC LIMIT 1;
+    `;
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    buscarTotalCartas,
+    buscarTotalCompra,
+    buscarTotalVenda,
+    buscarCartaMaisCara
 }
