dba577b639aa736042efebb143d7980bf265a8ba
matehus064 <matheus.drosa@sptech.school>
2026-07-17 06:04:10 -0300

    fix: removendo itens inutilizados (back-end)

--- src/controllers/avisoController.js
+++ src/controllers/avisoController.js
@@ -1,46 +1,5 @@
 var avisoModel = require("../models/avisoModel");
 
-function pesquisarDescricao(req, res) {
-    var descricao = req.params.descricao;
-
-    avisoModel.pesquisarDescricao(descricao)
-        .then(
-            function (resultado) {
-                if (resultado.length > 0) {
-                    res.status(200).json(resultado);
-                } else {
-                    res.status(204).send("Nenhum resultado encontrado!");
-                }
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-}
-
-function editar(req, res) {
-    var novaDescricao = req.body.descricao;
-    var idAviso = req.params.idAviso;
-
-    avisoModel.editar(novaDescricao, idAviso)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        )
-        .catch(
-            function (erro) {
-                console.log(erro);
-                console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
-
-}
-
 /*======================================
 |              ADAPTAÇÃO               |     
 ======================================*/
@@ -251,8 +210,6 @@ function listar(req, res) {
 module.exports = {
     listar,
     listarPorUsuario,
-    pesquisarDescricao,
     publicar,
-    editar,
     deletar
 }
--- src/routes/avisos.js
+++ src/routes/avisos.js
@@ -15,16 +15,8 @@ router.get("/listar/:idUsuario", function (req, res) {
     avisoController.listarPorUsuario(req, res);
 });
 
-router.get("/pesquisar/:descricao", function (req, res) {
-    avisoController.pesquisarDescricao(req, res);
-});
-
 router.post("/publicar/:idUsuario", function (req, res) {
     avisoController.publicar(req, res);
-});
-
-router.put("/editar/:idAviso", function (req, res) {
-    avisoController.editar(req, res);
 });
 
 router.delete("/deletar/:idAviso", function (req, res) {
